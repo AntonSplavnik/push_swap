99fb3cfb168e53c64d0cbbf7c71be5904133927e
LazyPixel <AntonSplavnik@gmail.com>
2024-11-11 17:49:03 +0100

    changed main

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antonsplavnik <antonsplavnik@student.42    +#+  +:+       +#+        */
+/*   By: asplavni <asplavni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 13:40:49 by asplavni          #+#    #+#             */
-/*   Updated: 2024/11/10 19:43:43 by antonsplavn      ###   ########.fr       */
+/*   Updated: 2024/11/11 17:45:10 by asplavni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,10 @@ int	main(int argc, char **argv)
 	t_stacks	stacks;
 
 	if (argc < 2)
-	{
 		ft_putstr("Error: Wrong number of arguments.\n"
 			"Should be more then two arguments.\n");
-	}
 	else
-	{
 		execution(argv, &stacks);
-	}
 	ft_putstr("\n");
 	return (0);
 }
